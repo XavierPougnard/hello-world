a94917d01cffc334803bc8398a88aebe8bc593a1
XavierPougnard <xavier.pougnard@orange.com>
2018-04-14 17:20:48 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -9,12 +9,15 @@ def hello():
 
 @application.route("/request_status")
 def answer():
-    return ("request status")
+    content_dic = {"bot id" : "ezy-park", "user": "Xavier"}
+    messageX = json.dumps(content_dic, indent=4)
+    return messageX
 	    
 @application.route("/completion")
 def answer2():
-    return "completion"
-
+    content_dic2 = {"bot id" : "ezy-park2", "user": "Xavier2"}
+    messageY = json.dumps(content_dic2, indent=4)
+    return messageY
 
 if __name__ == "__main__":
     application.run()

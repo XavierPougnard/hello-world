bdfe6b2cc028f538a034e77d40d7a8d41a7a06af
XavierPougnard <xavier.pougnard@orange.com>
2018-04-14 16:51:55 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,28 +1,20 @@
-#essai1
 import json
-from flask import Flask # pip install flask
-app = Flask(__name__)
+from flask import Flask
+application = Flask(__name__)
 
-@app.route("/")
+@application.route("/")
 def hello():
     return "eh oh"
 
 
-@app.route("/request_status")
+@application.route("/request_status")
 def answer():
-    content_dic = {"bot id" : "ezy-park", "user": "Xavier"}
-    messageX = json.dumps(content_dic, indent=4)
-    print(messageX)
-    return messageX
-
-	
-@app.route("/completion")
+    return ("request status")
+	    
+@application.route("/completion")
 def answer2():
-    content_dic2 = {"bot id" : "ezy-park2", "user": "Xavier2"}
-    messageY = json.dumps(content_dic2, indent=4)
-    print(messageY)
-    return messageY
+    return "completion"
 
 
 if __name__ == "__main__":
-    app.run()
+    application.run()

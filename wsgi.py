23a9eabf879a72fc07614cb823c295682f1e3eee
XavierPougnard <xavier.pougnard@orange.com>
2018-04-15 11:29:29 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -7,18 +7,31 @@ def hello():
     return "eh oh"
 
 
-@application.route("/payment/status", methods=['GET'])
-def expire_check():
+@application.route('/payment/status', methods=['GET'])
+'''def expire_check():
     #Check internal systems to determine if transactionId URL parameter is valid or expired
     #If valid, return 200 OK with valid status in JSON payload: {"status":"VALID"}
     #If expired, return 200 OK with expired status in JSON payload: {"status":"EXPIRED"}
     content = {'status':'VALID'}
     return content
-	    
-@application.route("/payment/pay", methods=['POST'])
-def payment_process():
+'''
+def answer():
+    content = {'status':'VALID'}
+    messageX = json.dumps(content, indent=4)
+    return messageX
+
+
+@application.route('/payment/pay', methods=['POST'])
+def answer2():
+    content = {'status':'SUCCESS'}
+    messageY = json.dumps(content, indent=4)
+    return messageY
+
+
+'''def payment_process():
     content = {'status':'SUCCESS'}
     return content
+'''
 
 if __name__ == "__main__":
     application.run()

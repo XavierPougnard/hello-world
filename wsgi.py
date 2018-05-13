605002d2928bd815f89ad41b5db105ad24597b1c
XavierPougnard <xavier.pougnard@orange.com>
2018-05-13 17:42:10 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -12,36 +12,8 @@ stripe.api_key = secret_key
 
 @application.route("/", methods=['GET'])
 def homepage():
-    #return render_template('index.html', pub_key=pub_key, amount=amount)    
-    return '''
-	<!DOCTYPE html>
-		<html>
-		  <head>
-		    <title>Ezy-Park</title>
-		    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-			<script src="JSlibrary.js" type="text/javascript"></script>
-		  </head>
-		  <body>
-		    Welcome to Ezy-Park !!!
-			<p>
-				<img src= 'very easy parking.png'>
-			</p>
-			<form action="payment/charge" method="POST">
-			  <script
-			    src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-			    data-key="pk_test_aw172A4CceQhwDIc55FJiF1J"
-			    data-amount="999"
-			    data-name="Xavier Pougnard"
-			    data-description="Example charge"
-			    data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
-			    data-locale="auto"
-			    data-currency="eur">
-			  </script>
-		       </form>
-		  </body>
-		</html>
-	'''
-
+    return render_template('index.html', pub_key=pub_key, amount=amount)    
+    
 @application.route('/payment/status', methods=['GET'])
     #Check internal systems to determine if transactionId URL parameter is valid or expired
     #If valid, return 200 OK with valid status in JSON payload: {"status":"VALID"}
